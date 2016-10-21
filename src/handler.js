10439edda5a317afcbffd12687f0850768e25720
Marco Lüthy <marco@luethy.net>
2016-10-21 02:47:34 +0100

    try to make it so you can deploy the function while using the sharp and libvips binaries from a linux environment (for lambda).. doesn't work yet

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,26 @@
 import processItem from './image'
 
 // eslint-disable-next-line import/prefer-default-export
-export async function processImage ({ Records: records }, context, callback) {
+export async function processImage (
+  { eventName, Records: records }, context, callback
+) {
+
+console.log('hey hey hey hey hey', eventName, records)
+
+  if (eventName.split(':')[0] !== 'ObjectCreated') {
+    console.error('Function was invoked without a valid event. Invoked by event name:', eventName)
+    return callback(
+      `Function was invoked without a valid event. Invoked by event name: ${eventName}`
+    )
+  }
+
   try {
     await Promise.all(records.map(processItem))
+console.log('promises! 19')
   } catch (error) {
     console.error(error)
     return callback(error, { records })
   }
-
+console.log('got here oh yea :23')
   return callback(null)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,24 @@
+const rimraf = require('rimraf')
+const targz = require('tar.gz')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const nodeExternals = require('webpack-node-externals')
+
+function SpecialPoop (archive, to) {
+  return {
+    apply: (compiler) => {
+      compiler.plugin('emit', (compilation, callback) => {
+        targz().extract(archive, to, (error) => {
+          if (error) {
+            return console.error('Unable to extract archive ', error.stack)
+          }
+
+          return callback()
+        })
+      })
+    },
+  }
+}
 
 module.exports = {
   entry: './src/handler',
@@ -19,6 +39,13 @@ module.exports = {
     path: '.webpack',
     filename: 'handler.js', // this should match the first part of function handler in serverless.yml
   },
-  externals: [nodeExternals()], // exclude external modules
-  //externals: ['sharp'],
+  //externals: [nodeExternals()], // exclude external modules
+  externals: ['sharp', 'aws-sdk'],
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: false, warnings: false }),
+    //new CopyWebpackPlugin([{ from: 'lib/sharp', to: './node_modules/sharp' }]), // copy the sharp and libvips pre-compiled for lambda execution environment
+    new SpecialPoop('lib/sharp-0.16.1-linux-x64.tar.gz', '.webpack/'),
+  ],
 }
