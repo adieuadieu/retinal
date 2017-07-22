e0a8cbe1d2d8cbd4115cb01b009cc3560280d075
Marco Lüthy <marco@luethy.net>
2017-07-22 20:42:37 +0200

    wip

--- src/handler.js
+++ src/handler.js
@@ -13,3 +13,16 @@ export async function processImage ({ Records: records }, context, callback) {
 
   return callback(null)
 }
+
+export async function metadata ({ Records: records }, context, callback) {
+  try {
+    await Promise.all(records.map(processItem))
+  } catch (error) {
+    console.error(error)
+    return callback(error, { records })
+  }
+
+  console.log(`Completed processing ${records.length} event${records.length === 1 ? '' : 's'}`)
+
+  return callback(null)
+}
